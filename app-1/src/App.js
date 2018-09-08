25f26faa1c713e2948ec7d3c99a56d84aa5be4d5
Evan Caldwell <evancald@sas.upenn.edu>
2018-09-08 12:46:15 -0600

    completed set 1, need to delete work in set 2 to finish #7

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      items: ['spruce', 'willow', 'pine', 'chestnut', 'locust'],
+      filteredItems: []
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  handleClick = () => {
+    const newItems = this.state.items.filter(item => item.includes(this.state.input));
+    this.setState({filteredItems: newItems, input: ''});
+  }
+
   render() {
+    let items = this.state.items.map((item, i) => {
+      return <div key={i}>{item}</div>
+    });
+    let filteredItems = this.state.filteredItems.map((item, i) => {
+      return <div key={i}>{item}</div>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div> 
+        <input onChange={this.handleChange} value={this.state.input} placeholder='search text'/>
+        <button onClick={this.handleClick}>Search</button>
+        {items}
+        <hr />
+        <h4>Search Results</h4>
+        {filteredItems}
       </div>
     );
   }
